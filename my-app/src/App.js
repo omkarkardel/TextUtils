7397b5f200444e2e49308b0ce487c1bea98e8b30
omkarkardel <omkarkardel175@gmail.com>
2026-01-09 13:51:46 +0530

    navbar color

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
+import react, { useState } from 'react';
 import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 function App() {
+  const[mode, setMode] = useState('dark');
   return (
     <>
-      <Navbar title="TextUtils" aboutText="aboutTextUtils" />
+      <Navbar title="TextUtils" aboutText="aboutTextUtils" mode={mode} />
 
       <div className="container my-3">
         <TextForm heading="Enter text to analyze below" />
